53116db758629851affc4ea605e895dcb89e3078
Peter Havens <peter.havens@gmail.com>
2016-07-20 11:19:37 -0600

    Account for pluralization of bottle in remaining bottles.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,11 @@
 class Bottles
   def verse(number)
+    next_number = number - 1
+    next_number_s = (next_number == 1) ? '' : 's'
+
     <<-END
 #{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+Take one down and pass it around, #{next_number} bottle#{next_number_s} of beer on the wall.
     END
   end
 end
